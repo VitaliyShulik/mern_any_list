86fdb546227ccd2b443e8a3d8911d26ebfbdb713
VitaliyShulik <you@example.com>
2020-06-25 10:42:14 +0600

    add filter for display not completed item first

--- client/src/components/AnyList.js
+++ client/src/components/AnyList.js
@@ -27,43 +27,71 @@ class AnyList extends Component {
 
     render() {
         const { items } = this.props.item;
+        
+        const notCompletedItem = items.map(({ _id, name, isCompleted }) => (
+            !isCompleted &&
+                (<CSSTransition key={_id} timeout={500} classNames="fade">
+                    <ListGroupItem className="list-item">
+                        <div className="item">
+                            { this.props.isAuthenticated ? 
+                                <input 
+                                    type="checkbox" 
+                                    className="item-checkbox"
+                                    checked={isCompleted}
+                                    onChange={this.onToggleIsCompletedClick.bind(this, _id, name, isCompleted)}
+                                /> : null}
+                                <span>{name}</span>
+                            { this.props.isAuthenticated ? <UpdateItemModal id={_id} name={name} isCompleted={isCompleted} /> : null }
+                        </div>
+                        { this.props.isAuthenticated ? 
+                            <Button
+                                className="remove-btn"
+                                color="danger"
+                                size="sm"
+                                onClick={this.onDeleteClick.bind(this, _id)}
+                            >
+                                &times;
+                            </Button> : null }
+                    </ListGroupItem>
+                </CSSTransition>)    
+            ));
+
+        const completedItem = items.map(({ _id, name, isCompleted }) => (
+            isCompleted &&
+                (<CSSTransition key={_id} timeout={500} classNames="fade">
+                    <ListGroupItem className="list-item">
+                        <div className="item">
+                            { this.props.isAuthenticated ? 
+                                <input 
+                                    type="checkbox" 
+                                    className="item-checkbox"
+                                    checked={isCompleted}
+                                    onChange={this.onToggleIsCompletedClick.bind(this, _id, name, isCompleted)}
+                                /> : null}
+                                <span 
+                                    style={{ textDecoration: 'line-through black' }}
+                                >{name}</span>
+                            { this.props.isAuthenticated ? <UpdateItemModal id={_id} name={name} isCompleted={isCompleted} /> : null }
+                        </div>
+                        { this.props.isAuthenticated ? 
+                            <Button
+                                className="remove-btn"
+                                color="danger"
+                                size="sm"
+                                onClick={this.onDeleteClick.bind(this, _id)}
+                            >
+                                &times;
+                            </Button> : null }
+                    </ListGroupItem>
+                </CSSTransition>)    
+            ));
        
         return(
             <Container>
                 <ListGroup> 
                     <TransitionGroup className="any-list">
-                        {items.map(({ _id, name, isCompleted }) => (
-                            <CSSTransition key={_id} timeout={500} classNames="fade">
-                                <ListGroupItem className="list-item">
-                                    <div className="item">
-                                        { this.props.isAuthenticated ? 
-                                            <input 
-                                                type="checkbox" 
-                                                className="item-checkbox"
-                                                checked={isCompleted}
-                                                onChange={this.onToggleIsCompletedClick.bind(this, _id, name, isCompleted)}
-                                            /> : null}
-                                            <span 
-                                                style={
-                                                    isCompleted ? 
-                                                    { textDecoration: 'line-through black' } : 
-                                                    { textDecoration: 'none' }
-                                                    }
-                                            >{name}</span>
-                                        { this.props.isAuthenticated ? <UpdateItemModal id={_id} name={name} isCompleted={isCompleted} /> : null }
-                                    </div>
-                                    { this.props.isAuthenticated ? 
-                                        <Button
-                                            className="remove-btn"
-                                            color="danger"
-                                            size="sm"
-                                            onClick={this.onDeleteClick.bind(this, _id)}
-                                        >
-                                            &times;
-                                        </Button> : null }
-                                </ListGroupItem>
-                            </CSSTransition>
-                        ))}
+                        { notCompletedItem }
+                        { completedItem }
                     </TransitionGroup>
                 </ListGroup>
             </Container>
